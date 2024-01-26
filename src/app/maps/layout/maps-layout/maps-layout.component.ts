c2f2ae876c46f48e098dc24692b0852b13130464
dotJManuel <dotJManuel@outlook.com>
2024-01-26 09:59:46 -0600

    creacion del menu

--- src/app/maps/layout/maps-layout/maps-layout.component.ts
+++ src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './maps-layout.component.html',
+  styleUrl: './maps-layout.component.css'
 })
 export class MapsLayoutComponent {
 
