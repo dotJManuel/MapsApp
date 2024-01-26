d6741ad6e3ca648567d5cca6ec4bb7baf03dec94
dotJManuel <dotJManuel@outlook.com>
2024-01-26 15:54:41 -0600

    implementacion de properties page

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {LngLat, Map, Marker} from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
+  styleUrl: './mini-map.component.css'
 })
 export class MiniMapComponent {
+
+    @Input() lngLat?: [number, number];
+    @ViewChild('map') divMap?: ElementRef;
+
+    ngAfterViewInit() {
+        if( !this.divMap?.nativeElement ) throw "Map Div not found";
+        if( !this.lngLat ) throw "LngLat can't be null";
+
+        const map = new Map({
+            container: this.divMap.nativeElement,//'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v12', // style URL
+            center: this.lngLat, // starting position [lng, lat]
+            zoom: 15, // starting zoom
+            interactive: false,
+        });
+
+        new Marker()
+            .setLngLat( this.lngLat )
+            .addTo( map )
+    }
 
 }
